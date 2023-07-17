04d419bb7a3423cfa2efa78245907f67bc2706ad
m.borbor <m.borbor@asiatech.ir>
2023-07-17 20:12:41 +0330

    models created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Index(models.Model):
+    ins_code = models.CharField(primary_key=True, max_length=20)
+    lval30 = models.CharField(max_length=25)
+
+class IndexHistory(models.Model):
+    index = models.ForeignKey(Index, on_delete=models.CASCADE)
+    date = models.DateTimeField(blank=True)
+    low = models.DecimalField(max_digits=8, decimal_places=1, blank=True)
+    high = models.DecimalField(max_digits=8, decimal_places=1, blank=True)
+    close = models.DecimalField(max_digits=8, decimal_places=1, blank=True)
+    
